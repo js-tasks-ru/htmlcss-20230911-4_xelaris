bb92d290cf3562e51b02721692ab936cb5f46431
alexey-ryabkov <xelaris@ya.ru>
2023-10-25 00:03:41 +0300

    setup building with postcss

---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  syntax: 'postcss-scss',
+  map: { inline: true },
+  plugins: [
+    require('precss')({
+      lookup: { disable: true },
+      properties: {
+        disable: true,
+        preserve: true,
+      },
+    }),
+    require('postcss-colour-functions'),
+    require('postcss-import-url')({
+      modernBrowser: true
+    }),
+    require('postcss-import'),
+    require('postcss-nested'),
+    require('autoprefixer'),
+    require('cssnano'),
+  ],
+}
