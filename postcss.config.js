5da5eac47fbb94a776e183b3740f4a0f8e3f847e
alexey-ryabkov <xelaris@ya.ru>
2023-11-06 22:40:42 +0300

    Improve postcss config, use vars and custom-media

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,27 @@
+const debug = !!process.env.DEBUG_CSS;
+
 module.exports = {
   syntax: 'postcss-scss',
   map: { inline: true },
   plugins: [
-    require('precss')({
-      lookup: { disable: true },
-      properties: {
-        disable: true,
-        preserve: true,
-      },
-    }),
-    require('postcss-colour-functions'),
+    require('postcss-at-debug')({ debug }),
+    require('postcss-advanced-variables'), 
     require('postcss-import-url')({
       modernBrowser: true
     }),
     require('postcss-import'),
-    require('postcss-nested'),
-    require('autoprefixer'),
+    require('postcss-utilities'),
+    require("postcss-sass-color-functions"),
+    require('postcss-map-get'),
+    require('postcss-preset-env')({
+      features: {
+				'nesting-rules': {
+					noIsPseudoSelector: false,
+				},
+        'media-query-ranges': true,
+        'custom-media-queries': true,
+			},
+    }),
     require('cssnano'),
   ],
 }
